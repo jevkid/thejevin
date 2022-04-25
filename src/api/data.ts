d8c4eb951ce6c133a6dd4143ef666fd7a96b5850
Megan Jevin <jevkid2@gmail.com>
2022-04-25 09:13:43 +0100

    Typos

--- src/api/data.ts
+++ src/api/data.ts
@@ -14,72 +14,7 @@ export const home = {
 };
 
 export const about = {
-  title: "About me",
-  subtitle: "Hello! I'm Megan, a Cardiff based Team Lead/Frontend Engineer at Crowdcube.",
-  content: [
-    {
-      title: "Leader",
-      body: `I currently manage a team of engineers, and love every minute of it.
-      Management is so different from engineering and requires such a wide
-      set of skills- I find myself incredibly lucky to be able to grow my
-      talents and abilities in such different areas. I truly enjoy helping
-      my devs not only day to day but also in their career progression
-      journey.`
-    },
-    {
-      title: "Engineer",
-      body: `In my dev time, I help design and implement beautiful, responsive,
-      accessible UIs. My main focus is always on the end user and UX is
-      always at the front of my mind. That being said, I'm also a stickler
-      for modular, reusable code and think that having a set of coding
-      practises within the team is invaluable. I'm a big fan of Agile
-      development and enjoy participating in all of the various SCRUM
-      ceremonies. In addition to this I'm well versed in planning,
-      gathering requirements, and refining tickets. I work closely with my
-      team and project manager to ensure that deliverables are met in a
-      timely and efficient manner.`
-    },
-    {
-      title: "Organiser",
-      body: `I tend to find myself at the centre of things and quite enjoy
-      organising everything from team lunches to office socials. I've also
-      gotten several of the companies I've worked for involved in
-      volunteer initiaties, mostly helping women and minorities learn to
-      code.`
-    },
-    {
-      title: "Student",
-      body: `I went to 3 universities before finally getting my degree, but being
-      an engineer means you're never not a "student". One thing I enjoy
-      about engineering is the fact that things are constantly changing-
-      what's the "best" today might not be the "best" tomorrow, and
-      frameworks/languages are always being changed and created.`
-    },
-    {
-      title: "Dog mom",
-      body: `I have two huge dogs who I love more than anything. They keep me
-      sane and are my reason for getting outdoors. Even though it was my
-      partner's idea to have two, I think I'm crazy enough to get a third
-      if we had the space...`
-    },
-    {
-      title: "Travel Enthusiast",
-      body: `Even though travelling has been on hold during the pandemic, I still
-      think about it constantly and am always planning one trip or
-      another. To this day I've been to 20+ countries and counting- my
-      favourite kind of trip is a city break with a carry on bag and my
-      camera!`
-    },
-    {
-      title: "Expat",
-      body: `I'm originally from Nashville, Tennessee. I moved to Wales to attend
-      Cardiff University, and was lucky enough to find a job that would
-      sponsor me after graduation. I've been here for going on 9 years and
-      have now obtained "Indefinite Leave to Remain", meaning I'm able to
-      reside in the UK permanently, which is a huge relief to my fiancee
-      and two pups!`
-    },
-  ]
+  title: "About me"
 }
 
 export const portfolio = {
@@ -102,7 +37,7 @@ export const portfolio = {
         src: "/images/StudentApp.jpg",
         subtitle: "Cardiff Student App",
         href: "https://www.cardiff.ac.uk",
-        description: "I single handedly engineered and developed Cardiff University's Student App, first in a third-party web wrapper using React, followed by React Native.",
+        description: "I single-handedly engineered and developed Cardiff University's Student App, first in a third-party web wrapper using React, followed by React Native.",
       },
       {
         src: "/images/CAA.jpg",
@@ -132,7 +67,7 @@ export const portfolio = {
         src: "/images/Wedding.jpg",
         subtitle: "Wedding Website",
         href: "https://www.github.com/jevkid/wedding-website",
-        description: "My partner and I's wedding website where guests will be able to RSVP, submit meal choices, dietary requirements, and find information related to the day. Built in Remix, React, TS, Prisma and MongoDB"
+        description: "My partner and my's wedding website where guests will be able to RSVP, submit meal choices, dietary requirements, and find information related to the day. Built in Remix, React, TS, Prisma and MongoDB"
       },
       {
         src: "/images/Murdle.jpg",
